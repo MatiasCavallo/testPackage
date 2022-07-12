1fd83ca88370540c43a5fcaf4bb294eeb8144c58
Matias Ezequiel Cavallo <65646120+MatiasCavallo@users.noreply.github.com>
2022-07-12 19:56:27 -0300

    add cli command

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const quotes = [
-  "Mati Funciona!",
-  "Funciona!",
-  "Funciona! Mati!",
-  "Algo nuevo aprendido",
-  "Todo por Mati L! 💪",
-];
+#!/usr/bin/env node
 
-/**
- * Gets a random Piñera Quote
- * @returns {string}
- */
-function randomQuote() {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
 
-module.exports = {
-  randomQuote
-};
+console.log(process.argv);
+
